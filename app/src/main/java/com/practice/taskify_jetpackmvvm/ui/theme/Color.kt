770c2abd2a2dc8b15ba1c3ed5b3112d0edb33174
abwaris19 <hellokhan.ch@gmail.com>
2023-11-27 21:27:06 +0100

    Added Custom Colors

--- app/src/main/java/com/practice/taskify_jetpackmvvm/ui/theme/Color.kt
+++ app/src/main/java/com/practice/taskify_jetpackmvvm/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val LowPriorityColor = Color(0xFFF000C980)
+val MediumPriorityColor = Color(0xFFFF9800)
+val HighPriorityColor = Color(0xFFE60C0C)
+val NonePriorityColor = Color(0xFFFFFFFF)
