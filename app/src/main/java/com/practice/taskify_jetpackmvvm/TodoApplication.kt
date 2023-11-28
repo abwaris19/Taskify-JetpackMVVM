3d9bc9e0d321e5b658c3cac99cd3ba4e510f9ed6
abwaris19 <hellokhan.ch@gmail.com>
2023-11-28 16:15:21 +0100

    SetUp Dagger-Hilt

---
+++ app/src/main/java/com/practice/taskify_jetpackmvvm/TodoApplication.kt
@@ -0,0 +1,9 @@
+package com.practice.taskify_jetpackmvvm
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class TodoApplication: Application() {
+}
---
+++ app/src/main/java/com/practice/taskify_jetpackmvvm/di/DatabaseModule.kt
@@ -0,0 +1,32 @@
+package com.practice.taskify_jetpackmvvm.di
+
+import android.content.Context
+import androidx.room.Room
+import com.practice.taskify_jetpackmvvm.TodoDatabase
+import com.practice.taskify_jetpackmvvm.util.constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context)
+    = Room.databaseBuilder(
+        context,
+        TodoDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+
+    @Singleton
+    @Provides
+    fun provideDao(database: TodoDatabase) = database.toDoDao()
+}
