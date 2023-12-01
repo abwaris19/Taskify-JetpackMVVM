2c9a1d4ec8fcd4a84fc74d9e302f7cb2cfe530f7
abwaris19 <hellokhan.ch@gmail.com>
2023-12-01 20:33:13 +0100

    Added List_Screen and Task_Screen constants

--- app/src/main/java/com/practice/taskify_jetpackmvvm/util/constants.kt
+++ app/src/main/java/com/practice/taskify_jetpackmvvm/util/constants.kt
@@ -4,4 +4,6 @@ object constants {
 
     const val DATABASE_TABLE = "TODO_TABLE"
     const val DATABASE_NAME =  "TODO_DATABASE"
+    const val LIST_SCREEN = "list/{action}"
+    const val TASK_SCREEN = "task/{taskId}"
 }
